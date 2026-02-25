81f97d108e8d7c5ca886558f0703bb39c8c4a89f
HandrinAmini <115077764+HandrinAmini@users.noreply.github.com>
2026-02-25 20:53:23 +0100

    Add files via upload

---
+++ kumaaap.js
@@ -0,0 +1,26 @@
+const gallery = document.getElementById('gallery'); 
+const popup = document.getElementById('popup');
+const selectedImage = document.getElementById('selectedImage'); 
+const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const selectedIndex = null;
+
+imageIndexes.forEach((i) => {
+    const image = document.createElement('img');
+    image.src = `./images/Kumamoto_${i}.jpg`;
+    image.alt = `Cover for Episode ${i} of the Compressed.fm Podcast`; 
+    image.classList.add('galleryImg');
+    
+    image.addEventListener('click', () =>{ // popup stuff
+        popup.style.transform = `translateY(0)`
+        selectedImage.src = `./images/Kumamoto_${i}.jpg`;
+        selectedImage.alt = `Cover for Episode ${i} of the Compressed.fm podcast`;
+    })
+
+    gallery.appendChild(image);
+})
+
+popup.addEventListener("click", () => {
+    popup.style.transform = `translateY(-100%)`;
+    popup.src = '';
+    popup.alt = '';
+})
